b8d254a91e24415d204bc09d09bdaeb3131c574d
mltrev23 <trevor.dev23@gmail.com>
2024-07-20 10:11:12 -0500

    feat: load model and tokenizer & test

--- main.py
+++ main.py
@@ -20,3 +20,22 @@ for i, index in enumerate(example_indices):
     print(dataset['test'][index]['summary'])
     print(dash_line)
     print()
+
+model_name = 'google/flan-t5-base'
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = True)
+
+sentence = 'What time is it, Tom?'
+
+sentence_encoded = tokenizer(sentence, return_tensors = 'pt')
+
+sentence_decoded = tokenizer.decode(
+    sentence_encoded['input_ids'][0],
+    skip_special_tokens = True
+)
+
+print('Encoded sentence:')
+print(sentence_encoded['input_ids'][0])
+print('\nDecoded sentence:')
+print(sentence_decoded)
